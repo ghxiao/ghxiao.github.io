9ca919895f273dbb4256b75be4ac83697e8290bf
Guohui Xiao <gh.xiao@gmail.com>
2015-12-05 18:44:10 +0100

    update publications

--- _includes/upp.sh
+++ _includes/upp.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 export TMPDIR=.
-bib2bib  -c 'author:"xiao"' ~/Dropbox/Papers/biblio.bib > xiao.bib
+#bib2bib  -c 'author:"xiao"' ~/Dropbox/Papers/biblio.bib > xiao.bib
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 biber pub_body
+htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 
