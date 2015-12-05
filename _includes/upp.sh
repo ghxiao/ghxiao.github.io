c3135a4cc4e97bda9e6607ba5f5467c148c2357b
Guohui Xiao <gh.xiao@gmail.com>
2015-12-05 17:49:46 +0100

    AAAI 2016 paper

--- _includes/upp.sh
+++ _includes/upp.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+export TMPDIR=.
+bib2bib  -c 'author:"xiao"' ~/Dropbox/Papers/biblio.bib > xiao.bib
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 biber pub_body
