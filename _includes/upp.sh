d24217c65f22928a7860768d2a1e09ad78ee8690
Guohui Xiao <gh.xiao@gmail.com>
2016-03-19 16:31:53 +0100

    agile and dl 2016 papers

--- _includes/upp.sh
+++ _includes/upp.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export TMPDIR=.
 #bib2bib  -c 'author:"xiao"' ~/Dropbox/Papers/biblio.bib > xiao.bib
+rm *.4ct *.4tc *.aux *.bbl *.bcf *.blg *.idv *.div *.lg *.log *.run.xml *.tmp *.xref
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 htlatex pub_body.tex  "xhtml, charset=utf-8" " -cunihtf -utf8"
 biber pub_body
